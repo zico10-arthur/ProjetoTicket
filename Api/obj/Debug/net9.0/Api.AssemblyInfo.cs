c6ae3bbdd6af27c141b1b0074c6bdfdf24beaa88
Eduardo Leal <eduardoleal.ferreir4@gmail.com>
2026-03-12 15:57:26 -0300

    ajeitando pastas

--- Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f251808db7c988fcc94907bcca71e31d7b1528c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc5b3498ac19f3df5b07f63c626ccb9c063a758")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
