3e826aed1e3040e8fd61fff63a9f3814406ac7ef
zico10-arthur <arthurrezendetere@gmail.com>
2026-03-14 16:55:06 -0300

    feat:  configura injeção de dependência e repositório de usuários

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,14 @@
+using Infraestructure.Repository;
+using Domain.Interface;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
 
 var app = builder.Build();
 
