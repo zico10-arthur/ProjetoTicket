80201215c4ebc2f5c890dfc21d0f55bcf87a0439
zico10-arthur <arthurrezendetere@gmail.com>
2026-03-25 15:11:40 -0300

    feature front do login e cadastro prontos

--- Api/Program.cs
+++ Api/Program.cs
@@ -23,10 +23,20 @@ builder.Services.AddScoped<ConnectionFactory>(sp =>
     return new ConnectionFactory(connectionString!);
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Front", policy =>
+        policy.WithOrigins("http://localhost:5057")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors("Front");
 
 app.UseHttpsRedirection();
 
