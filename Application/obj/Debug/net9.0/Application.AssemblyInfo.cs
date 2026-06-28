e48ff8f3743a8d83d9057866f727bb1ffef7955a
Eduardo Leal <eduardoleal.ferreir4@gmail.com>
2026-06-28 16:05:59 -0300

    .

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc4f38ac3a99baf84b188dfeff4dfbb06f58aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1333202288694a9a69fee9527df2f34f9998663")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
